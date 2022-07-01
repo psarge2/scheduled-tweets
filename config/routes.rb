8a4b1127e1e02b69a90b439677b2194bd397660a
psarge2 <paul@psarge.dev>
2022-07-01 11:02:20 -0400

    created sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
-  # get "/"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#creasate"
+  
   root to: "main#index"
 end
